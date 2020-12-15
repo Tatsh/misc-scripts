eb4357b374cfe8a406ed384cb5db9eeb5d705490
Andrew Udvare <audvare@gmail.com>
2020-12-15 15:51:21 -0500

    Add a2gif

--- misc_scripts/media/__init__.py
+++ misc_scripts/media/__init__.py
@@ -1,3 +1,4 @@
+from .a2gif import main as a2gif
 from .add_cdda_time import main as add_cdda_time
 from .audio2vid import main as audio2vid
 from .encode_dashcam import main as encode_dashcam
@@ -6,5 +7,5 @@ from .mp4json import main as mp4json
 from .mvid_rename import main as mvid_rename
 from .umpv import main as umpv
 
-__all__ = ('add_cdda_time', 'audio2vid', 'encode_dashcam', 'flacted',
+__all__ = ('a2gif', 'add_cdda_time', 'audio2vid', 'encode_dashcam', 'flacted',
            'mp4json', 'mvid_rename', 'umpv')
