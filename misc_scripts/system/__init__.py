6ba3e32b0ad3aa6feccc608a957fe799c9bc4dfb
Andrew Udvare <audvare@gmail.com>
2020-12-18 10:54:00 -0500

    Move text commands to text-utils

--- misc_scripts/system/__init__.py
+++ misc_scripts/system/__init__.py
@@ -1,6 +1,6 @@
 from .dtree import main as dtree
 from .find_empty_dirs import main as find_empty_dirs
 from .nss_add_cert import main as nss_add_cert
+from .slugrename import main as slugrename
 
-
-__all__ = ('dtree', 'find_empty_dirs', 'nss_add_cert')
+__all__ = ('dtree', 'find_empty_dirs', 'nss_add_cert', 'slugrename')
