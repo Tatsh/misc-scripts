ce3a1f4360acf8cbc56a9515d7fce8fd4ab6b667
Andrew Udvare <audvare@gmail.com>
2024-09-25 02:17:12 -0400

    Add generate_chrome_user_agent()

--- tatsh_misc_utils/string.py
+++ tatsh_misc_utils/string.py
@@ -1,15 +1,19 @@
 from collections.abc import Iterator, Sequence
-from functools import cache
+from functools import cache, lru_cache
 from pathlib import Path
+from typing import cast
 import os
 import re
+
+import requests
 
 from .itertools import chunks
 from .system import IS_WINDOWS
 from .typing import StrPath
 
-__all__ = ('hexstr2bytes', 'hexstr2bytes_generator', 'is_ascii', 'strip_ansi',
-           'strip_ansi_if_no_colors', 'underscorize', 'unix_path_to_wine')
+__all__ = ('generate_chrome_user_agent', 'get_latest_chrome_major_version', 'hexstr2bytes',
+           'hexstr2bytes_generator', 'is_ascii', 'strip_ansi', 'strip_ansi_if_no_colors',
+           'underscorize', 'unix_path_to_wine')
 
 ORD_MAX = 128
 STRIP_ANSI_PATTERN = re.compile(r'\x1B\[\d+(;\d+){0,2}m')
@@ -59,3 +63,18 @@ def unix_path_to_wine(path: StrPath) -> str:
     except FileNotFoundError:
         path = Path.cwd() / path
     return f'Z:{path}'.replace('/', '\\')
+
+
+@lru_cache
+def get_latest_chrome_major_version() -> str:
+    return cast(
+        str,
+        requests.get(
+            'https://versionhistory.googleapis.com/v1/chrome/platforms/win/channels/stable/versions',
+            timeout=5).json()['versions'][0]['version'].split('.')[0])
+
+
+@lru_cache
+def generate_chrome_user_agent(os: str = 'Windows NT 10.0; Win64; x64') -> str:
+    return (f'Mozilla/5.0 ({os}) AppleWebKit/537.36 (KHTML, like Gecko) '
+            f'Chrome/{get_latest_chrome_major_version()}.0.0.0 Safari/537.36')
