8257862a95e88a4350ac6cdf3b9dfc03a7774825
Andrew Udvare <audvare@gmail.com>
2024-10-06 02:12:08 -0400

    Improve accuracy of pydbus stubs

--- .stubs/pydbus/bus.pyi
+++ .stubs/pydbus/bus.pyi
@@ -1,6 +1,35 @@
-from typing import Any, Literal, overload
+from typing import Any, Literal, TypedDict, overload
 
 from gi.repository.Gio import DBusConnection
+
+OrgBluezDict = TypedDict(
+    'OrgBluezDict', {
+        'org.bluez.AgentManager1': dict[Any, Any],
+        'org.bluez.ProfileManager1': dict[Any, Any],
+        'org.freedesktop.DBus.Introspectable': dict[Any, Any]
+    })
+OrgBluesHci0Dict = TypedDict('OrgBluesHci0Dict', {'org.bluez.Adapter1': dict[str, Any]})
+
+DBusOrgFreedesktopDBusObjectManagerManagedObjectsDict = TypedDict(
+    'DBusOrgFreedesktopDBusObjectManagerManagedObjectsDict',  # noqa: PYI053
+    {
+        '/org/bluez': OrgBluezDict,
+        '/org/bluez/hci0': OrgBluesHci0Dict
+    },
+    total=False)
+
+
+class DBusOrgFreedesktopDBusObjectManager:
+    @staticmethod
+    def GetManagedObjects() -> DBusOrgFreedesktopDBusObjectManagerManagedObjectsDict:  # noqa: N802
+        ...
+
+
+class Bluez:
+    def __getitem__(
+            self, key: Literal['org.freedesktop.DBus.ObjectManager']
+    ) -> DBusOrgFreedesktopDBusObjectManager:
+        ...
 
 
 class Notifications:
@@ -9,7 +38,7 @@ class Notifications:
     def UnInhibit(self, key: int) -> int | None:  # noqa: N802
         ...
 
-    def Inhibit(self, unk1: str, unk2: str, unk3: Any) -> int | None:  # noqa: N802
+    def Inhibit(self, name: str, reason: str, unk1: Any) -> int | None:  # noqa: N802
         ...
 
 
@@ -19,6 +48,10 @@ class Bus:
     @overload
     def get(self, domain: Literal['org.freedesktop.Notifications'],
             path: Literal['/org/freedesktop/Notifications']) -> Notifications:
+        ...
+
+    @overload
+    def get(self, domain: Literal['org.bluez'], path: Literal['/']) -> Bluez:
         ...
 
     @overload
