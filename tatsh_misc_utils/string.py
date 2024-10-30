983126795a000030bed5dfa17b718a734319ce7d
Andrew Udvare <audvare@gmail.com>
2024-10-30 05:56:20 -0400

    Add faster way to get Chrome major version

--- tatsh_misc_utils/string.py
+++ tatsh_misc_utils/string.py
@@ -97,20 +97,53 @@ def unix_path_to_wine(path: StrPath) -> str:
 
 
 @cache
+def get_last_chrome_major_version() -> str:
+    """
+    Get last major Chrome version from the profile directory.
+    
+    Tries the following, in order:
+
+    * Chrome Beta
+    * Chrome
+    * Chrome Canary
+    * Chromium
+
+    Returns
+    -------
+    str
+        If no ``Last Version`` file is found, returns empty string.
+    """
+    for location in ('~/.config/google-chrome-beta', '~/AppData/Local/Google/Chrome Beta/User Data'
+                     '~/Library/Application Support/Google/Chrome Beta', '~/.config/google-chrome',
+                     '~/AppData/Local/Google/Chrome/User Data',
+                     '~/Library/Application Support/Google/Chrome',
+                     '~/.config/google-chrome-unstable',
+                     '~/AppData/Local/Google/Chrome Canary/User Data',
+                     '~/Library/Application Support/Google/Chrome Canary', '~/.config/chromium',
+                     '~/AppData/Local/Google/Chromium/User Data',
+                     '~/Library/Application Support/Google/Chromium'):
+        if (p := (Path(location).expanduser() / 'Last Version')).exists():
+            return p.read_text().split('.', 1)[0]
+    return ''
+
+
+@cache
 def get_latest_chrome_major_version() -> str:
     """Get the latest Chrome major version."""
     return cast(
         str,
         requests.get(
-            'https://versionhistory.googleapis.com/v1/chrome/platforms/win/channels/stable/versions',
+            ('https://versionhistory.googleapis.com/v1/chrome/platforms/win/channels/stable/'
+             'versions'),
             timeout=5).json()['versions'][0]['version'].split('.')[0])
 
 
 @cache
 def generate_chrome_user_agent(os: str = 'Windows NT 10.0; Win64; x64') -> str:
     """Get a Chrome user agent."""
-    return (f'Mozilla/5.0 ({os}) AppleWebKit/537.36 (KHTML, like Gecko) '
-            f'Chrome/{get_latest_chrome_major_version()}.0.0.0 Safari/537.36')
+    last_major = get_last_chrome_major_version() or get_latest_chrome_major_version()
+    return (f'Mozilla/5.0 ({os}) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/{last_major}.0.0.0'
+            ' Safari/537.36')
 
 
 @cache
