39a953dc2cabc78061d50c03d77102469fde0c55
Andrew Udvare <audvare@gmail.com>
2024-11-16 20:03:19 -0500

    Formatting

--- .stubs/gi/repository/Gio.pyi
+++ .stubs/gi/repository/Gio.pyi
@@ -4,7 +4,6 @@ from typing import Any
 from gi.overrides.GLib import Variant
 
 class DBusConnection:
-    
     def signal_subscribe(self, unk1: Any, iface: str, signal: str, unk2: Any, unk3: Any, unk4: int,
                          handler: Callable[[Any, Any, str, Any, Any, Variant], None]) -> None:
         ...
